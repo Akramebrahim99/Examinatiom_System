d2da079c9497b91dea9ca1a4ab778525cadcb71d
eslamamged74 <solomamged74@gmail.com>
2021-05-18 04:37:58 +0200

    register validate

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -15,7 +16,7 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
         
-        $this->validate($request,[
+        /*$this->validate($request,[
             'name' => 'required',
             'email' => 'required',
             'pass' => 'required',
@@ -24,7 +25,26 @@ class RegisterController extends Controller
             'university' => 'required',
             'collage' => 'required',
             'phone-num' => 'required'
-        ]);
+        ]);*/
+        $rules= [
+            'name' => 'required|max:191|unique:students,name|string',
+            'email' => 'required',
+            'pass' => 'required',
+            'id' => 'required|numeric|unique:students,id',
+            'ssn' => 'required|numeric|unique:students,ssn',
+            'university' => 'required|string',
+            'collage' => 'required|string',
+            'phone-num' => 'required|numeric|unique:students,phone'
+        ];
+
+        $messages = $this->getMessages();
+
+        $validator = Validator::make($request->all(),$rules,$messages);
+        
+        if($validator -> fails()){
+            return redirect()->back()->withErrors($validator)->withInputs($request->all());
+        }
+
 
         $student = new Student([
             'name' => $request->get('name'),
@@ -38,8 +58,21 @@ class RegisterController extends Controller
         ]);
 
         $student->save();
-
-        return redirect()->route('login');
+        return redirect()->back()->with(['success' => 'YOU Are Member Now Congrtulations']);
+       // return redirect()->route('login');
        
+    }
+
+    protected function getMessages(){
+        return $messages = [
+            'name.required' => 'your name is required please',
+            'email.required' => 'your email is required please',
+            'pass.required' => 'your password is required please',
+            'id.required' => 'your id is required please',
+            'ssn.required' => 'your ssn is required please',
+            'university.required' => 'your university name is required please',
+            'collage.required' => 'your collage name is required please',
+            'phone-num.required' => 'your phone number is required please',
+        ];
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -22,38 +22,68 @@
                 <div class="signup-content">
                     <div class="signup-form">
                         <h2 class="form-title">{{__('massages.Register')}}</h2>
+                        @if (Session::has('success'))
+                            <div class="alert alert-success" role="alert">
+                             {{Session::get('success')}}
+                            </div>
+                        @endif
+                        <br>
                         <form method="POST" class="register-form" id="register-form" action="{{route('store.data')}}">
                         @csrf
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="name" id="name" placeholder="Name"/>
+                                @error('name')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Email"/>
+                                @error('email')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
                                 <input type="password" name="pass" id="pass" placeholder="Password"/>
+                                @error('pass')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="id" id="name" placeholder="Id"/>
+                                @error('id')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="ssn" id="name" placeholder="SSN"/>
+                                @error('ssn')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="university" id="name" placeholder="University"/>
+                                @error('university')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div><div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="collage" id="name" placeholder="Collage"/>
+                                @error('collage')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="phone-num" id="name" placeholder="Phone Number"/>
+                                @error('phone-num')
+                                <small class="form-text text-danger">{{$message}}</small>
+                                @enderror
                             </div>
                             <div class="form-group form-button">
                                 <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
